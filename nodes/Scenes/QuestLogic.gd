3dd3a6a722b6d66b6eb02ae6d7d29bba0df1fa83
FruitSoil <derevo1q@gmail.com>
2026-05-22 03:30:49 +0300

    Description unlock cond. fix

--- nodes/Scenes/QuestLogic.gd
+++ nodes/Scenes/QuestLogic.gd
@@ -5,7 +5,6 @@ extends Node
 func _ready() -> void:
 	var tutor = load("res://resources/Quests/TutorialPart1.tres")
 	ql_wait_to_next(3, tutor, Global.workers.ADMIN)
-	print(tutor)
 
 ##Добавить квест типа new_quest персонажу worker через wait_time секунд
 func ql_wait_to_next(wait_time: float, new_quest: Quest, worker: Global.workers) -> void:
