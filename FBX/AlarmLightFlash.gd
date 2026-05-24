070c4c33866911e196a002b2844150f5be12e72c
timdobychin-afk <tim.dobychin@gmail.com>
2026-05-24 17:14:15 +0300

    Terrain, monolithLumen, alarmworks,

--- FBX/AlarmLightFlash.gd
+++ FBX/AlarmLightFlash.gd
@@ -1,11 +1,5 @@
 extends OmniLight3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	var time = Time.get_ticks_msec() / 1000.0
+	var wave = sin(time * 4.0) * 5.0
+	light_energy = wave + 5
