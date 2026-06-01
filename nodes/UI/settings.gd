56d743bddf1e51e7f542524815d8c128e62e08bf
FruitSoil <derevo1q@gmail.com>
2026-06-01 19:54:42 +0300

    Fonts

--- nodes/UI/settings.gd
+++ nodes/UI/settings.gd
@@ -1,29 +1,35 @@
 extends CanvasLayer
 
 @onready var exit: Button = $Exit
-
 @onready var master_percent: Label = $Sounds/Master/Master/Percent
-
+@onready var percents: Array[Label]
 @onready var sliders: Array[HSlider] = [
-	$Sounds/Master/MasterSlider
+	$Sounds/Master/MasterSlider,
+	
 ]
 
 func _ready() -> void:
-	var index: int = -1
-	
-	for slider in sliders:
-		index += 1
-		slider.value_changed.connect(change_volume.bind(index))
-		slider.value = AudioServer.get_bus_volume_db(index)
+	for i in range(sliders.size()):
+		var bus_db_value = AudioServer.get_bus_volume_db(i)
+		sliders[i].value = bus_db_value
+		sliders[i].value_changed.connect(change_volume.bind(i))
+		
+		if i == 0:
+			update_percent_label(bus_db_value)
 	
 	exit.pressed.connect(_on_button_pressed)
 	hide()
 
-func change_volume(value: int, ID: int):
-	match ID:
-		1:
-			AudioServer.set_bus_volume_db(0,value)
-			master_percent.text = str(value)
+func change_volume(value: float, bus_id: int) -> void:
+	AudioServer.set_bus_volume_db(bus_id, value)
+	
+	if bus_id == 0:
+		update_percent_label(value)
+
+func update_percent_label(db_value: float) -> void:
+	var linear_val = db_to_linear(db_value)
+	var percentage = round(linear_val * 100)
+	master_percent.text = str(percentage)
 
 func _on_button_pressed() -> void:
 	hide()
