20556a075071d187f29aee64b50c28bb9f506a73
FruitSoil <derevo1q@gmail.com>
2026-06-02 02:35:52 +0300

    TweenAnimComponent fix

--- nodes/Scenes/tws_button.gd
+++ nodes/Scenes/tws_button.gd
@@ -5,6 +5,7 @@ class_name CompJuice
 @export var ease_: Tween.EaseType = Tween.EASE_OUT
 @export var time: float = 0.07
 @export var scale_value: Vector2 = Vector2(1.1,1.1)
+@export var small_scale_value: Vector2 = Vector2(1.1,1.1)
 @export var init_scale: Vector2 = Vector2(1,1)
 
 @onready var control: Control = get_parent()
@@ -15,18 +16,17 @@ func _ready() -> void:
 	control.mouse_entered.connect(_on_mouse_hovered.bind(true))
 	control.mouse_exited.connect(_on_mouse_hovered.bind(false))
 
-
 func _on_mouse_hovered(is_hovered: bool):
 	if set_tween():
+		if control is BaseButton:
+			if control.disabled:
+				tw.tween_property(control, "scale", small_scale_value if is_hovered else init_scale, time)
+			if control.has_meta("buy"):
+				if control.get_parent().is_open == false:
+					tw.tween_property(control, "scale", small_scale_value if is_hovered else init_scale, time)
 		tw.tween_property(control, "scale", scale_value if is_hovered else init_scale, time)
 
 func set_tween() -> bool:
-	if control is BaseButton:
-		if control.disabled:
-			return false
-		elif control.has_meta("buy"):
-			if control.get_parent().is_open == false:
-				return false
 	if tw:
 		tw.kill()
 	tw = create_tween().set_ease(ease_).set_trans(trans).set_parallel(true)
